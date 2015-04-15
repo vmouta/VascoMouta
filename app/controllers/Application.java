bdedae0407849a7e34d631dc01d08ad1ae4a4c26
Vasco Mouta <vasco.mouta@gmail.com>
2015-04-15 17:08:45 +0200

    Add multi-lang (missing translations)

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,29 +1,72 @@
 package controllers;
 
+import static play.data.Form.form;
 import play.*;
+import play.i18n.Lang;
 import play.mvc.*;
+import play.mvc.Http.Cookie;
+import utils.AppLogger;
+import utils.Cookies;
 import views.html.*;
 
 public class Application extends AppController {
 
-    public static Result index() {
-        return ok(index.render("Vasco Mouta"));
+	public static Result goHome() {
+		return indexI18n(lang().code());
+	}
+	
+	public static Result index() {
+        return i18nRedirect("");
     }
     
-    public static Result protfolio() {
+    public static Result indexI18n(String lang) {
+    	return redirect("/"+ lang + "/home.html");
+    }
+    
+    public static Result i18nRedirect(String uri) {
+    	Cookie langCookie = request().cookie(Cookies.PLAY_LANG);
+    	if(langCookie == null) {
+    		String lang = Lang.preferred(request().acceptLanguages()).code();
+    		AppLogger.debugLog("Set application language to " + lang);
+    		changeLang(lang);
+    	} else if(findLanguage(langCookie.value()) == null) {
+    		String lang = Lang.preferred(request().acceptLanguages()).code();
+    		AppLogger.debugLog("Language cookie not found set language to " + lang);
+    		changeLang(lang);
+    	} else {
+    		AppLogger.debugLog("Cookie Language " + langCookie.value());
+    	}
+        return redirect("/"+ lang().code() + "/"+ uri);
+    }
+    
+    public static Result home(String lang) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/home.html");
+    	return ok(index.render("Vasco Mouta"));
+    }
+    
+    public static Result protfolio(String lang) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/protfolio.html");
         return ok(portfolio.render());
     }
     
-    public static Result resume() {
+    public static Result resume(String lang) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/resume.html");
         return ok(resume.render());
     }
     
-    public static Result blog() {
+    public static Result blog(String lang) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/blog.html");
         return ok(contact.render());
     }
     
-    public static Result contact() {
+    public static Result contact(String lang) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/contact.html");
         return ok(contact.render());
+    }
+    
+    public static Result routerI18n(String lang, String uri) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/" + uri);
+    	return notFound();
     }
     
 	/* Expoused routes to javascript */
--- app/controllers/Portfolio.java
+++ app/controllers/Portfolio.java
@@ -6,47 +6,58 @@ import views.html.portfolios.*;
 
 public class Portfolio extends AppController {
 	
-	public static Result alianca() {
+	public static Result alianca(String lang) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/alianca.html");
         return ok(alianca.render());
     }
 	
-	public static Result beetagg() {
+	public static Result beetagg(String lang) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/beetagg.html");
         return ok(beetagg.render());
     }
 	
-    public static Result boat24() {
+    public static Result boat24(String lang) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/boat24.html");
         return ok(boat24.render());
     }
     
-    public static Result caravan24() {
+    public static Result caravan24(String lang) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/caravan24.html");
         return ok(caravan24.render());
     }
     
-    public static Result irma() {
+    public static Result irma(String lang) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/irma.html");
         return ok(irma.render());
     }
     
-    public static Result kilimosalama() {
+    public static Result kilimosalama(String lang) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/kilimosalama.html");
         return ok(kilimosalama.render());
     }
     
-    public static Result localina() {
+    public static Result localina(String lang) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/localina.html");
         return ok(localina.render());
     }
     
-    public static Result premfire() {
+    public static Result premfire(String lang) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/premfire.html");
         return ok(premfire.render());
     }
     
-    public static Result skimondo() {
+    public static Result skimondo(String lang) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/skimondo.html");
         return ok(skimondo.render());
     }
     
-    public static Result surfkitchen() {
+    public static Result surfkitchen(String lang) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/surfkitchen.html");
         return ok(surfkitchen.render());
     }
     
-    public static Result zeerio() {
+    public static Result zeerio(String lang) {
+    	if(needLanguageRedirect(lang)) return redirect("/"+ lang().code() + "/zeerio.html");
         return ok(zeerio.render());
     }
 }
